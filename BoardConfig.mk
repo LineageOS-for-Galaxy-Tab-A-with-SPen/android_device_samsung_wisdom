a4a3b65777b01d5aab29b7d30a6fb6cd8e03ea75
eun0115 <gianpaoloestacio5@gmail.com>
2025-10-02 09:36:22 +0800

    wisdom: Drop BOARD_KERNEL_SEPARATED_DT
    - dtbhtoolExynos: unable to locate any dtbs in the given path
    
    Signed-off-by: eun0115 <gianpaoloestacio5@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 DEVICE_PATH := device/samsung/wisdom
 
 # Kernel
-BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_CONFIG := wisdom_defconfig
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
